fe23af214f2db30cf8fcf2dc5746ced6f0635ca1
Eric Fischer <ericf712@gmail.com>
2015-06-16 17:23:56 -0700

    Add stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,28 @@
 
 // but you don't so you're going to write it from scratch:
 
-var stringifyJSON = function(obj) {
-  // your code goes here
+var stringifyJSON = function (obj) {
+  if (typeof obj === "string") {return '"' + obj + '"';}
+  if (obj === null) {return "" + obj;}
+
+  if(Array.isArray(obj)){
+    var results = obj.map(function(item){
+      return stringifyJSON(item);
+    }).join(",");
+    return "["+ results + "]";
+  }
+
+  if(typeof obj === "object") {
+    var results = [];
+    for (var key in obj) {
+      if (typeof obj[key] === "function" || typeof obj[key] === undefined) {
+        return "{}";
+      } else {
+       results.push(stringifyJSON(key) + ":" + stringifyJSON(obj[key]));
+     }
+    }
+    return "{" + results.join(",") + "}";
+  }
+
+  return "" + obj; // For numbers and booleans
 };
